ea43c574cbf454ef1c77de65cd2ab226a92ed175
Rebeca Marques <rebecarmsg@gmail.com>
2022-02-23 14:35:26 -0300

    @RebecaMarquesSilva [Update - Consumo de API]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { UserLogin } from '../model/UserLogin';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastrar',
@@ -8,12 +11,40 @@ import { UserLogin } from '../model/UserLogin';
 })
 export class CadastrarComponent implements OnInit {
 
-  userLogin: UserLogin = new UserLogin
+  user: User = new User
+  confirmaSenha: string
+  tipoUsuario: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     window.scroll(0,0)
   }
 
+  confirmSenha(event: any) {
+    this.confirmaSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar() {
+    this.user.tipo = this.tipoUsuario
+
+
+  if(this.user.senha != this.confirmaSenha) {
+    alert("As senhas estão incorretas")
+  } else {
+    this.authService.cadastrar(this.user).subscribe((resp: User) => {
+      this.user = resp
+      this.router.navigate(["/entrar"])
+
+      alert("Usuário cadastrado com sucesso!")
+    })
+  }
+  }
 }
