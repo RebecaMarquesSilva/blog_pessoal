625124da4bb468ac5edb3ad72a60c380418ba44c
Rebeca Marques <rebecarmsg@gmail.com>
2022-04-02 22:01:58 -0300

    @RebecaMarquesSilva [ Add Ajustes Finais ]

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 
@@ -17,7 +18,8 @@ export class CadastrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -37,13 +39,13 @@ export class CadastrarComponent implements OnInit {
 
 
   if(this.user.senha != this.confirmaSenha) {
-    alert("As senhas estão incorretas")
+    this.alertas.showAlertDanger("As senhas estão incorretas")
   } else {
     this.authService.cadastrar(this.user).subscribe((resp: User) => {
       this.user = resp
       this.router.navigate(["/entrar"])
 
-      alert("Usuário cadastrado com sucesso!")
+      this.alertas.showAlertSuccess("Usuário cadastrado com sucesso!")
     })
   }
   }
--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { UserLogin } from '../model/UserLogin';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -15,7 +16,8 @@ export class EntrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -39,7 +41,7 @@ export class EntrarComponent implements OnInit {
     error: erro => {
 
       if(erro.status == 401) {
-      alert("Usuário e/ou senha estão inválidos!")
+        this.alertas.showAlertDanger("Usuário e/ou senha estão inválidos!")
       }
     }
   })
